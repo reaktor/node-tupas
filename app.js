1e8db61cc96251c2a0c1909b89dc5d2874014b46
Laura Leppänen <laura.leppanen@reaktor.fi>
2014-01-21 14:40:54 +0200

    SSL configuration for test app (add your own keys and certs).

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var tupas = require('./tupas');
-var app = express();
+var express = require('express')
+  , https = require('https')
+  , fs = require('fs')
+  , tupas = require('./tupas')
+  , app = express();
 
 tupas.initialize("xxxxxxxxxxx", app, "https://localhost:8080", "/tupas", handler);
 
@@ -9,4 +11,12 @@ function handler(tupasStatus, responseData) {
   console.log(responseData);
 }
 
-app.listen(8080)
+var sslOptions = {
+  key: fs.readFileSync('../../certs/server.key'),
+  cert: fs.readFileSync('../../certs/server.crt'),
+  ca: fs.readFileSync('../../certs/ca.crt'),
+  requestCert: true,
+  rejectUnauthorized: false
+};
+
+https.createServer(sslOptions, app).listen(8080);
