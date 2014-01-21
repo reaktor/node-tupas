b6c96a4690aa2dac5243b28e7957e1c42a300ff5
Juho Salmio <juho.salmio@gmail.com>
2014-01-21 13:26:30 +0200

    fix indentatation

--- tupas.js
+++ tupas.js
@@ -1,4 +1,4 @@
-var crypto = require ("crypto");
+var crypto = require("crypto");
 var request = require("superagent");
 var jade = require("jade");
 var moment = require("moment");
@@ -10,9 +10,9 @@ var rejectPath = "/reject";
 exports.initialize = function (vendorId, appHandler, hostUrl, baseUrl, callback) {
 
   var returnUrls = {
-      ok : hostUrl + baseUrl + okPath,
-      cancel : hostUrl + baseUrl + cancelPath,
-      reject : hostUrl + baseUrl + rejectPath
+    ok: hostUrl + baseUrl + okPath,
+    cancel: hostUrl + baseUrl + cancelPath,
+    reject: hostUrl + baseUrl + rejectPath
   }
 
   appHandler.get("/form", form(vendorId, "FI", returnUrls));
@@ -22,51 +22,51 @@ exports.initialize = function (vendorId, appHandler, hostUrl, baseUrl, callback)
 }
 
 function form(vendorId, languageCode, returnUrls) {
-   return function(req, res) {
-       var now = moment().format('YYYYMMDDhhmmss');
-       var bankParams = config.banks.map(function(bank) {
-         var params = {
-            bankAuthUrl : bank.authUrl,
-            messageType : "701",
-            version : bank.version,
-            vendorId : vendorId,
-            identifier : now + "123456",
-            languageCode : languageCode,
-            idType : bank.idType,
-            returnLink : returnUrls.ok,
-            cancelLink : returnUrls.cancel,
-            rejectLink : returnUrls.reject,
-            keyVersion : bank.keyVersion,
-            algorithmType : "03",
-            checksumKey: "xxxxxxxxxxxxxxxxx"
-          }
-          var mac = generateMAC(params);
-          params.mac = mac;
-          return params;
-       });
+  return function (req, res) {
+    var now = moment().format('YYYYMMDDhhmmss');
+    var bankParams = config.banks.map(function (bank) {
+      var params = {
+        bankAuthUrl: bank.authUrl,
+        messageType: "701",
+        version: bank.version,
+        vendorId: vendorId,
+        identifier: now + "123456",
+        languageCode: languageCode,
+        idType: bank.idType,
+        returnLink: returnUrls.ok,
+        cancelLink: returnUrls.cancel,
+        rejectLink: returnUrls.reject,
+        keyVersion: bank.keyVersion,
+        algorithmType: "03",
+        checksumKey: "xxxxxxxxxxxxxxxxx"
+      }
+      var mac = generateMAC(params);
+      params.mac = mac;
+      return params;
+    });
 
-       var html = jade.renderFile('./views/form.jade', {banks : bankParams});
-       res.send(html);
-    }
+    var html = jade.renderFile('./views/form.jade', {banks: bankParams});
+    res.send(html);
+  }
 }
 
 function generateMAC(p) {
-    var macParams = [
-        p.messageType,
-        p.version,
-        p.vendorId,
-        p.languageCode,
-        p.identifier,
-        p.idType,
-        p.returnLink,
-        p.cancelLink,
-        p.rejectLink,
-        p.keyVersion,
-        p.algorithmType,
-        p.checksumKey]
+  var macParams = [
+    p.messageType,
+    p.version,
+    p.vendorId,
+    p.languageCode,
+    p.identifier,
+    p.idType,
+    p.returnLink,
+    p.cancelLink,
+    p.rejectLink,
+    p.keyVersion,
+    p.algorithmType,
+    p.checksumKey]
 
-    var joinedParams =  macParams.join("&") + "&";
-    return crypto.createHash('sha256').update(joinedParams).digest('hex');
+  var joinedParams = macParams.join("&") + "&";
+  return crypto.createHash('sha256').update(joinedParams).digest('hex');
 }
 
 function ok(callback) {
