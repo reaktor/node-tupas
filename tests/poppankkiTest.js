c29e2bddffe77f269fbd306e14e4910b85ac9016
Hannu Arvila <hannu.arvila@reaktor.fi>
2014-07-01 09:19:54 +0300

    Add option to authenticate using POP Pankki

---
+++ tests/poppankkiTest.js
@@ -0,0 +1,34 @@
+var config = require('../config.json');
+
+casper.test.begin('POP Pankki Authentication', 1, function(test) {
+  casper.start('https://localhost:' + config.port, function() {
+    this.click('#poppankki-login');
+  });
+
+  casper.then(function() {
+    this.fill('form[name="LoginForm"]', {
+      "USERNAME" : "11111111",
+      "PASSWORD" : "123456"
+    }, true);
+  });
+
+  casper.then(function () {
+    this.fill('form[name="SecurityKeyForm"]', {
+      "SECURITYKEY" : '123456'
+    }, true);
+  });
+
+  casper.then(function () {
+    this.click('button.SubmitButton');
+  });
+
+  casper.waitForSelector('#success', function() {
+    test.assertExists("#success");
+    this.echo("Succesfully authenticated with POP Pankki");
+  }, function(){}, 10000);
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
