83089f2aba5df73fe1d65819ec1f47a7e34ee1a0
Benjamin Robson <unoceptik@gmail.com>
2017-08-24 11:31:31 +0300

    Update start-sample.js
    I think this would add clarification - I'm pretty junior so please correct me if this is unnecessary :)

--- sample/start-sample.js
+++ sample/start-sample.js
@@ -1,3 +1,4 @@
 var app = require('./app');
 var config = require('../config.json');
+console.log('Running on https://localhost:8081/'); // port can be changed in '../config.json'
 app.listen(config.port);
