922c0a08eb28621fda716724856cdf1dfd7ce0b3
Lauri Lehmijoki <lauri.lehmijoki@iki.fi>
2017-11-07 10:50:29 +0200

    Print sample server URL at startup

--- sample/start-sample.js
+++ sample/start-sample.js
@@ -1,3 +1,9 @@
 var app = require('./app');
 var config = require('../config.json');
-app.listen(config.port);
+app.listen(config.port, err => {
+  if (err) {
+    console.error(err)
+  } else {
+    console.log('Started sample server at https://localhost:' + config.port)
+  }
+});
